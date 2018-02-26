ce1fb5c9c85ac0f2baa86f1b6461988dcebe5297
Lucio <git@xinfab.com>
2018-02-26 17:25:26 +0000

    Initial commit after migration to new git repo

---
+++ gantt.js
@@ -0,0 +1,25 @@
+var ganttData = [
+	{
+		id: 1, name: "Fab Academy", series: [
+			{ name: "Course duration", start: new Date(2017,00,25), end: new Date(2017,05,21) },
+			
+            { name: "Skecth", start: new Date(2017,00,25), end: new Date(2017,00,31), color: "#f0f0f0" },
+            
+            { name: "Initial Design", start: new Date(2017,01,01), end: new Date(2017,01,07), color: "#f0f0f0" },
+            
+            { name: "Make frame and parts", start: new Date(2017,01,08), end: new Date(2017,02,12), color: "#f0f0f0" },
+			
+            { name: "Design Electronics", start: new Date(2017,02,01), end: new Date(2017,02,15), color: "#f0f0f0" },
+            
+            { name: "Make Electronics", start: new Date(2017,02,16), end: new Date(2017,03,07), color: "#f0f0f0" },
+            
+            { name: "Program electronics", start: new Date(2017,03,08), end: new Date(2017,04,13), color: "#f0f0f0" },
+            
+            { name: "Assembly", start: new Date(2017,04,14), end: new Date(2017,05,13), color: "#f0f0f0" },
+            
+            { name: "Project presentation", start: new Date(2017,05,14), end: new Date(2017,05,21), color: "#f0f0f0" },
+            
+		]
+	}, 
+	
+];
